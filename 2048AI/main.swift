9b1bd0319491069274bddd58838221e7b13a80f4
Aaron Kaufer <electricaaron.com@gmail.com>
2018-12-30 08:35:31 +0100

    Initial Commit

---
+++ 2048AI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  2048AI
+//
+//  Created by Aaron Kaufer on 12/30/18.
+//  Copyright © 2018 Aaron Kaufer. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
